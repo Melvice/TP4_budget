80198e15e248c37fe68de913ee565fa2c9f4d47d
Melvice Guimfack <melvice-junior.guimfack@polymtl.ca>
2023-03-22 16:07:42 -0400

    modif myScript.py

--- myscript.py
+++ myscript.py
@@ -1,8 +1,11 @@
 import os
 import sys
 
+# Obtention des arguments bad_commit et good_commit depuis la ligne de commande
+bad_commit = 'c1a4be04b972b6c17db242fc37752ad517c29402'
+good_commit = 'e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c' 
 # Définition de la commande à exécuter
-command = "git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
+command = "git bisect start %s %s" % (bad_commit, good_commit)
 
 # Exécution de la commande
 os.system(command)
