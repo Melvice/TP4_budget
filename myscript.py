9be6c6bbfb81cef03984dfff41b019bda5d796d0
Melvice Guimfack <melvice-junior.guimfack@polymtl.ca>
2023-03-22 17:02:32 -0400

    changes

--- myscript.py
+++ myscript.py
@@ -3,7 +3,7 @@ import os
 bad_commit = 'c1a4be04b972b6c17db242fc37752ad517c29402'
 good_commit = 'e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c'
 os.system('git bisect start')
-os.system('git bisect bad %s' % (bad_commit))
-os.system('git bisect good %s' % (good_commit))
+os.system('git bisect bad c1a4be04b972b6c17db242fc37752ad517c29402')
+os.system('git bisect good e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c')
 os.system('git bisect run python manage.py test ')
 os.system('git bisect reset')
