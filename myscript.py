e6ec0ecbe6f1dbfcdcf9c733e3d822e5d10721d2
Melvice Guimfack <melvice-junior.guimfack@polymtl.ca>
2023-03-24 11:47:56 -0400

    modif fichier script

--- myscript.py
+++ myscript.py
@@ -1,7 +1,15 @@
 import os
-import sys
+
+#remarque pour entrer manuellement les commits dans 
+#la ligne de commande, on pourrait plutôt faire comme suit: 
+#import sys
+#bad_commit = sys.argv[1]
+#good_commit = sys.argv[2]
+
+#Définition des commits
 bad_commit = "c1a4be04b972b6c17db242fc37752ad517c29402"
 good_commit = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
+#Excéctution des commandes git bisect
 os.system('git bisect start %s %s' % (bad_commit, good_commit))
 os.system('git bisect run python manage.py test ')
 os.system('git bisect reset')
